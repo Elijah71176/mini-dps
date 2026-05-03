e8c6d098150af0083e2544273b75b1912801d13f
Bamidele Mayowa A elijah <elijah71176@gmail.com>
2026-05-03 17:57:19 +0200

    Fix TypeORM SSL config for CI and AWS RDS

--- apps/api/src/typeorm.config.ts
+++ apps/api/src/typeorm.config.ts
@@ -2,15 +2,21 @@ import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import { Customer } from '../src/customers/customers.entity';
 import { Project } from '../src/projects/project.entity';
+import { ServiceRequest } from '../src/request/request.entity';
+
+const isAwsRds =
+  process.env.DATABASE_URL?.includes('rds.amazonaws.com') ?? false;
 
 export default new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  
-  ssl: {
-    rejectUnauthorized: false,
-  },
 
-  entities: [Customer, Project],
+  ssl: isAwsRds
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
+
+  entities: [Customer, Project, ServiceRequest],
   migrations: ['src/migrations/*.ts'],
 });
